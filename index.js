83ac4b7380313c2ce9fb593a0b21ef8d9d04c98e
Seunggyu <seunggyul14@icloud.com>
2022-11-25 16:43:44 -0800

    commit tree 2f2fdf65738a2ba9bc535139224c7796f7205965

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//1
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+//2
+const random = Math.round(Math.random());
+console.log(random);
+
+//3
+const num3 = 16
+const num4 = 6
+
+let mod = num3 % num4
+
+//4
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,)
